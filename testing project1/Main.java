54bc895579fb8a4a839d206eba869c6588473377
ashraf amad <ashraf_amad@hotmail.com>
2019-03-17 14:28:53 +0200

    Create Main.java

---
+++ testing project1/Main.java
@@ -0,0 +1,13 @@
+package com.ashraf.junit.helper;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		StringHelper h = new StringHelper();
+		System.out.println(h.truncateAInFirst2Positions("AACCC"));
+		
+
+	}
+
+}
