da3e95c0b49941c738806b3a6acb565d7a598e10
ashraf amad <ashraf_amad@hotmail.com>
2019-03-24 14:10:56 +0200

    Create StringHelperTest.java

---
+++ testing project1/StringHelperTest.java
@@ -0,0 +1,44 @@
+package com.ashraf.junit.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	// AACD => CD 
+	//ACD => CD 
+	//CDEF=>CDEF 
+	//CDAA => CDAA
+
+	StringHelper helper;
+	
+	
+	@Before
+	public void before(){
+		helper = new StringHelper();
+		System.out.println("@before....");
+	}
+	
+
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirst2Positions() 
+	{
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+		//assertEquals(expected, actual)
+	
+	}
+
+	
+	
+
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirstPosition() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+
+
+
+	
+}
